3aa270ca0badbd31871cd0e6629a4d6bad138022
chaoky <levimanga@gmail.com>
2024-06-25 23:04:54 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ];
+        };
+      };
+
+      packages.${system} = {
+        default = pkgs.hello;
+      };
+
+      formatter = pkgs.nixfmt;
+    };
+}
